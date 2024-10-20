da83aef507996920a43a5449a25dbccc185f6cfa
nathanjpaek <46549297+Pathanaek@users.noreply.github.com>
2024-10-19 21:23:33 -0700

    all functions

--- bayes_net.py
+++ bayes_net.py
@@ -2,7 +2,7 @@
 # r_i - number of instantiations of x_i
 # q_i - number of parental instantiations of x_i
 # m_ijk - number of times x_i takes on kth instantiation and parents take on jth instantiation
-# P(G|D) proportional P(G)P(D|G)
+# P(G|D) proportional to P(G)P(D|G)
 
 import sys
 import networkx as nx
@@ -38,15 +38,15 @@ def calculate_m_ijk(df, dag, x_i, rv_dict):
         parent_instantiation_to_j = {parent_values_j: j for j, parent_values_j in enumerate(possible_parent_instantiations)}
         m_ijk = np.zeros((q_i, r_i), dtype=int)
 
-        # Group data by parents and x_i
+        # group data by parents and x_i
         grouped = df.groupby(parents_of_xi + [x_i]).size()
         for index_tuple, count in grouped.items():
-            parent_values = tuple(index_tuple[:-1])  # Parent values as a tuple
-            x_i_value = index_tuple[-1]             # Value of x_i
+            parent_values = tuple(index_tuple[:-1])  # parent values as a tuple
+            x_i_value = index_tuple[-1]             # value of x_i
             j_index = parent_instantiation_to_j[parent_values]
             k_index = value_k_to_k_index[x_i_value]
             m_ijk[j_index, k_index] = count
-
+            
     return m_ijk, q_i, r_i
 
 
@@ -69,11 +69,7 @@ def calculate_r_i(x_i, rv_dict):
     return r_i, values_xi
 
 
-def calculate_theta_ijk():
-    # CODE HERE
-    return 
-
-
+# create a dict with x_i's mapped to their possible values
 def map_rvs_to_possible_values(infile):
     df = pd.read_csv(infile)
     rv_names = list(df)
@@ -83,14 +79,13 @@ def map_rvs_to_possible_values(infile):
     return rv_dict
 
 
+# get K2 score with uniform prior 
 def get_score(dag, df, rv_dict):
     score = 0
     for x_i in dag.nodes():
         parents_of_xi = list(dag.predecessors(x_i))
         m_ijk, q_i, r_i = calculate_m_ijk(df, x_i, parents_of_xi, rv_dict)
-        
-        # For variables with no parents
-        if q_i == 1:
+        if q_i == 1: # if no parents
             m_ij = np.sum(m_ijk)
             score += math.lgamma(r_i) - math.lgamma(r_i + m_ij)
             for k in range(r_i):
@@ -104,25 +99,61 @@ def get_score(dag, df, rv_dict):
     return score
 
 
+# generate candidates by add, remove, reverse operations
+def get_next_graphs(dag, rv_list):
+    next_dags = []
+    for u in rv_list:
+        for v in rv_list:
+            if u != v:
+                if not dag.has_edge(u, v): # add
+                    temp_dag = dag.copy()
+                    temp_dag.add_edge(u, v)
+                    if nx.is_directed_acyclic_graph(temp_dag):
+                        next_dags.append(temp_dag)
+                if dag.has_edge(u, v): # remove
+                    temp_dag = dag.copy()
+                    temp_dag.remove_edge(u, v)
+                    next_dags.append(temp_dag)
+                if dag.has_edge(v, u): # reverse
+                    temp_dag = dag.copy()
+                    temp_dag.remove_edge(v, u)
+                    temp_dag.add_edge(u, v)
+                    if nx.is_directed_acyclic_graph(temp_dag):
+                        next_dags.append(temp_dag)
+    return next_dags
+
+
+# algorithm 5.3 in the book 
+def fit(dag, df, rv_dict, rv_list):
+    max_iterations = 20
+    attempt = 0
+    best_score = get_score(dag, df, rv_dict)
+    best_dag = dag.copy()
+    while attempt < max_iterations:
+        attempt += 1
+        improved = False
+        candidate_dags = get_next_graphs(best_dag, rv_list)
+        for candidate_dag in candidate_dags:
+            candidate_score = get_score(candidate_dag, df, rv_dict)
+            if candidate_score > best_score:
+                best_score = candidate_score
+                best_dag = candidate_dag.copy()
+                improved = True
+                break
+        if not improved:
+            break 
+    return best_dag
+
 
 def compute(infile, outfile):
-    # rv_dict = map_rvs_to_possible_values(infile)
+    rv_dict = map_rvs_to_possible_values(infile)
     df = pd.read_csv(infile)
     rv_list = list(df.columns)
     dag = nx.DiGraph(name=infile)
-
-    for rv in rv_list: # create a base graph from the rvs read from file
+    for rv in rv_list: 
         dag.add_node(rv)
-
-    # optimize the graph
-    init_score = get_score(dag, df)
-    best_dag = dag.copy()
-    max_iterations = 30
-    i = 0
-    for node in best_dag.nodes():
-        # greedy iteration over the graph looking for a better shape than current
-
-    pass
+    best_dag = fit(dag, df, rv_dict, rv_list)
+    write_gph(best_dag, {name: name for name in best_dag.nodes()}, outfile)
 
 
 def main():
